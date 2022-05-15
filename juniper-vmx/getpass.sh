4d2933e5b12bb7b4d577b39a0f43472e1c5bd295
Kostas Zorbadelos <kzorba@nixly.net>
2022-05-15 20:32:59 +0200

    Added Juniper vMX configurations for our lab topology.
    We use https://github.com/Juniper/OpenJNPR-Container-vMX that provides Juniper
    vMX as a docker container. You have to provide JunOS images and licences
    independently. The documentation in the above repo mentions all requirements and
    you have to read it.

---
+++ juniper-vmx/getpass.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2017, Juniper Networks, Inc.
+# All rights reserved.
+#
+# simple script to extract root passwords from vmx log file
+
+if [ "$USER" == "root" ]; then
+  CLI=cli
+fi
+
+list=$(docker ps --format '{{.Names}}' | grep ^vmx)
+for vmx in $list; do
+  descr=$(docker logs $vmx | grep 'root password' | cut -d' ' -f1-5,8 | tr -d '\r' || echo $vmx)
+  if [ ! -z "$descr" ]; then
+    ip=$(docker logs $vmx | grep 'root password'| awk -F'v4:' '{print $2}' | awk '{print $1}')
+    fpcmem=$(ssh -o StrictHostKeyChecking=no -o ConnectTimeout=1 -i ../napalm-ssh-keys/lab_id_rsa user@$ip $CLI show chassis fpc 0 2>/dev/null | grep Online | awk '{print $9}')
+    fpcmem="${fpcmem:-0}"
+    if [ "$fpcmem" -gt "256" ]; then
+      success=$(($success + 1))
+      echo -e "$descr \t ready"
+    else
+      echo -e "$descr \t ..."
+    fi
+  fi
+done
